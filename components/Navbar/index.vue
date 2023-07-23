e785da7a6dcb24880dbed70d7db9ddfa2e9147c7
sipamungkas <sipamungkas.dev@gmail.com>
2023-07-23 22:22:10 +0700

    fix: navbar

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -11,8 +11,10 @@ import {
 
 
 const mobileMenuOpen = ref(false)
-const localPath = useLocalePath()
-const switchLocale = useSwitchLocalePath()
+const localePath = useLocalePath()
+const switchLocalePath = useSwitchLocalePath()
+
+
 
 </script>
 
@@ -20,7 +22,7 @@ const switchLocale = useSwitchLocalePath()
     <header class="bg-white">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
-                <NuxtLink :to="localPath('/dashboard')">
+                <NuxtLink :to="localePath('/dashboard')">
                     <span class="sr-only">Expensee - a minimalist expense tracker</span>
                     <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> -->
                     <span class="font-semibold text-lg lg:text-2xl">Expensee</span>
@@ -34,7 +36,7 @@ const switchLocale = useSwitchLocalePath()
                 </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12 mt-1">
-                <NuxtLink href="#"
+                <NuxtLink :to="localePath('/dashboard')"
                     class="text-sm lg:text-md 2xl:text-base font-semibold leading-6 text-gray-900 hover:text-gray-900 capitalize">
                     {{ $t('dashboard') }}</NuxtLink>
                 <NuxtLink href="#"
@@ -65,7 +67,7 @@ const switchLocale = useSwitchLocalePath()
                 <DialogPanel
                     class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div class="flex items-center justify-between">
-                        <NuxtLink href="localPath('/dashboard')" class="-m-1.5 p-1.5">
+                        <NuxtLink href="localePath('/dashboard')" class="-m-1.5 p-1.5">
                             <span class="sr-only">Expensee - a minimalist expense tracker</span>
                             <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                             alt="" /> -->
@@ -80,24 +82,24 @@ const switchLocale = useSwitchLocalePath()
                         <div class="-my-6 divide-y divide-gray-500/10">
                             <div class="space-y-2 py-6">
 
-                                <NuxtLink :to="localPath()"
+                                <NuxtLink :to="localePath()"
                                     class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                     {{ $t('dashboard') }}</NuxtLink>
-                                <NuxtLink :to="localPath()"
+                                <NuxtLink :to="localePath()"
                                     class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                     {{ $t('accounts') }}</NuxtLink>
-                                <NuxtLink :to="localPath()"
+                                <NuxtLink :to="localePath()"
                                     class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                     {{ $t('records') }}</NuxtLink>
-                                <NuxtLink :to="localPath()"
+                                <NuxtLink :to="localePath()"
                                     class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                     {{ $t('analytics') }}</NuxtLink>
-                                <NuxtLink :to="localPath()"
+                                <NuxtLink :to="localePath()"
                                     class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                     {{ $t('imports') }}</NuxtLink>
                             </div>
                             <div class="py-6">
-                                <NuxtLink :to="localPath()"
+                                <NuxtLink :to="localePath()"
                                     class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                     Log in</NuxtLink>
                             </div>
