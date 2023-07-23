3b425344279509f866a24b63e6fe5bb7908f1ea3
sipamungkas <sipamungkas.dev@gmail.com>
2023-07-23 22:22:47 +0700

    feat(components): add AccountItem component

---
+++ components/AccountItem/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+    title: string,
+    balance: number,
+    class: any
+}
+const props = defineProps<Props>()
+
+const classValue = computed(() => (props.class ? props.class : 'bg-purple-400'))
+
+</script>
+
+
+<template>
+    <div class="flex flex-row px-4 py-2 items-center gap-3 rounded-xl w-[100%] md:w-[30%] xl:w-[20%]" :class="classValue">
+        <slot />
+        <div>
+            <h4 class="text-white/80 font-bold">{{ title }}</h4>
+            <span class="text-white font-semibold">IDR {{ balance }}K</span>
+        </div>
+    </div>
+</template>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,8 +1,15 @@
 <script setup>
-
+import { BanknotesIcon, CreditCardIcon } from '@heroicons/vue/24/solid'
 </script>
 
 <template>
-    <Navbar />
-    <h1>tes</h1>
+    <div class="flex flex-1 flex-wrap gap-2">
+        <AccountItem title="Cash" balance="2000" class="bg-orange-400">
+            <BanknotesIcon class='h-8 w-8 text-white' />
+        </AccountItem>
+        <AccountItem title="Cash" balance="2000">
+            <BanknotesIcon class='h-8 w-8 text-white' />
+        </AccountItem>
+
+    </div>
 </template>
